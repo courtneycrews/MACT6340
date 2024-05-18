cd500d65b013bf67ff37af8476dd391a23e49fcd
courtneycrewsu <crews@smu.edu>
2024-05-18 16:13:54 -0700

    added contact form part 1

---
+++ public/js/contact.js
@@ -0,0 +1,10 @@
+(function () {
+    "use strict";
+
+    document.querySelector("#contact-form-button").addEventListener("click", submitMail);
+
+    function submitMail() {
+        console.log("Thank you for your message.")
+    }
+    
+})();
