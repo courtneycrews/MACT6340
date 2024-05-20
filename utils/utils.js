6c630bdda503723328b918d9a788416d88c31214
courtneycrewsu <crews@smu.edu>
2024-05-20 13:36:06 -0700

    got form to work with gmail

--- utils/utils.js
+++ utils/utils.js
@@ -1,22 +1,34 @@
 import nodemailer from "nodemailer";
 
 export async function sendMessage(sub, txt) {
+    console.log("MAIL_HOST:", process.env.MAIL_HOST);
+    console.log("MAIL_PORT:", process.env.MAIL_PORT);
+    console.log("MAIL_SECURE:", process.env.MAIL_SECURE);
+    console.log("MAIL_USERNAME:", process.env.MAIL_USERNAME);
+    console.log("MESSAGE_FROM:", process.env.MESSAGE_FROM);
+    console.log("MESSAGE_TO:", process.env.MESSAGE_TO);
+
     let transporter = nodemailer.createTransport({
         host: process.env.MAIL_HOST,
-        port: process.env.MAIL_PORT,
-        secure: process.env.MAIL_SECURE,
+        port: parseInt(process.env.MAIL_PORT, 10),
+        secure: process.env.MAIL_SECURE === 'true',
         auth: {
             user: process.env.MAIL_USERNAME,
             pass: process.env.MAIL_PASSWORD,
         },
-        requireTLS: process.env.MAIL_TLS,
+        requireTLS: process.env.MAIL_TLS === 'true',
     });   
     let message = {
         from: process.env.MESSAGE_FROM,
-        to: process.env.MESSAGE_TO,
+        to: 'courtneymichelle.cc@gmail.com',  // process.env.MESSAGE_TO,
         subject: sub,
         text: txt,
     };
+
+    if (!message.to) {
+        console.error("No recipients defined.");
+        return;
+    }
     
     await transporter
         .sendMail(message)
