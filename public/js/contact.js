5df158cecfd8e5eb5bd34f2ffd38f508350fae29
courtneycrewsu <crews@smu.edu>
2024-05-20 02:01:10 -0700

    set up nodemailer part 3

--- public/js/contact.js
+++ public/js/contact.js
@@ -3,9 +3,7 @@
 
     let form = document.querySelector('#contact-form')
 
-    document
-    .querySelector("#contact-form-button")
-    .addEventListener("click", (event) => {
+    document.querySelector("#send-contact").addEventListener("click", (event) => {
         event.preventDefault();
         event.stopPropagation();
         let formValid = true;
@@ -21,13 +19,29 @@
 
     function sendTheEmail() {
         console.log("Thank you for your message.");
-        let firstName = document.querySelector("#first-name").value;
-        let lastName = document.querySelector("#last-name").value;
-        let email = document.querySelector("#mail").value;
-        let message = document.querySelector("#msg").value;
-        console.log("First name: " + firstName);
-        console.log("Last name: " + lastName);
-        console.log("Email: " + email);
-        console.log("Message: " + message);
+        let obj = {
+            sub: "New Message from Reader",
+            txt: `${document.querySelector("#contact-first").value}
+            ${document.querySelector("#contact-last").value} sent a message that reads
+            ${document.querySelector("#contact-msg").value}. They're email address is
+            ${document.querySelector("#contact-email-addr").value}`,
+        };
+
+        fetch("/mail", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(obj),
+        })
+        .then((r) => r.json())
+        .then((response) => {
+            document.querySelector("#contact-button-response").innerHTML = response.result;
+        })
+        .then(() => {
+            setTimeout(() => {
+                document.querySelector("#contact-button-response").innerHTML = "";
+            }, "5000");
+        });
     }       
 })();
