c2f38e06ead5c517410ab732c78e9fcffeb53b58
courtneycrewsu <crews@smu.edu>
2024-05-27 13:55:54 -0700

    Completed ejs part 3, project gallery page functioning correctly

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import * as utils from "./utils/utils.js";
 dotenv.config();
+let data = ["Project 1", "Project 2", "Project 3"];
 
 const app = express();
 const port = 3000;
@@ -9,16 +10,20 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/index", (req, res) => {
+app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.get("/featured", (req, res) => {
-  res.render("featured.ejs");
+app.get("/gallery", (req, res) => {
+  res.render("gallery.ejs", { projectArray: data });
 });
 
-app.get("/gallery", (req, res) => {
-  res.render("gallery.ejs");
+app.get("/artwork/:id", (req, res) => {
+  let id = req.params.id;
+  if (id > data.length) {
+    throw new Error("No artwork exists");
+}
+res.render("artwork.ejs", { projectArray: data, which: id });
 });
 
 app.get("/contact", (req, res) => {
@@ -34,6 +39,11 @@ app.post("/mail", async (req, res) => {
   .catch(() => {
     res.send({ result: "failure" });
   });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.render("error.ejs");
 });
 
 app.listen(port, () => {
