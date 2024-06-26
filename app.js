315b1756989f98b6dd7cc46b5f95147ac91ecbfd
courtneycrewsu <crews@smu.edu>
2024-06-25 22:07:57 -0700

    updates packages installed snyk with no vulnerabilities

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 import express from "express";
-import dotenv from "dotenv";
 import * as utils from "./utils/utils.js";
-dotenv.config();
 import * as db from './utils/database.js';
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+
+
 let data = ["Project 1", "Project 2", "Project 3"];
 let projects = [];
 
 const app = express();
+app.use(cors());
 const port = 3000;
 app.set("view engine", "ejs");
 app.use(express.json());
